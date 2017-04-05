5913247456700b097b2d83bfe39a3a93994e99ec
Ville Loic <lville@SA203-07A.ensi-bourges.fr>
2017-04-05 12:21:56 +0200

    oui

--- ProjectIvy/src/example/Hello.java
+++ ProjectIvy/src/example/Hello.java
@@ -2,13 +2,19 @@
 package example;
 
 import org.apache.commons.lang.WordUtils;
+import com.opencsv.CSVReader;
+import java.io.*;
 
 public class Hello {
     public static void main(String[] args) {
+    	int monmax=0;
         String  message = "hello ivy !";
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+        CSVReader reader = new CSVReader(new FileReader("data.csv"));
+        List<String[]> myEntries = reader.readAll();
+        System.out.println("mon max: " + monmax);
     }
 }
         
