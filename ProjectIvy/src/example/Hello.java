f4b6b1331677b3e6225be5bc9650e47d876d81bc
Ville Loic <lville@SA203-07A.ensi-bourges.fr>
2017-04-11 15:30:42 +0200

    enfin la fin de ce programme particulierement desagreable a debugger

--- ProjectIvy/src/example/Hello.java
+++ ProjectIvy/src/example/Hello.java
@@ -1,20 +1,33 @@
 
 package example;
 
-import org.apache.commons.lang.WordUtils;
-import com.opencsv.CSVReader;
 import java.io.*;
+import org.apache.commons.lang.WordUtils;
+import java.util.*;
+import com.opencsv.*;
 
 public class Hello {
     public static void main(String[] args) {
     	int monmax=0;
         String  message = "hello ivy !";
         System.out.println("standard message : " + message);
-        System.out.println("capitalized by " + WordUtils.class.getName() 
-                                + " : " + WordUtils.capitalizeFully(message));
-        CSVReader reader = new CSVReader(new FileReader("data.csv"));
-        List<String[]> myEntries = reader.readAll();
+        System.out.println("capitalized by " + WordUtils.class.getName() + " : " + WordUtils.capitalizeFully(message));
+        try {
+            CSVReader reader = new CSVReader(new FileReader("data.csv"),',');
+            try{
+                List<String[]> myEntries=reader.readAll();
+                for (String[] strings : myEntries){
+                    System.out.println(Arrays.toString(strings));
+                    for(String nombre : strings){
+                        monmax = max(monmax, Integer.parseInt(nombre));
+                        }                        
+                    }
+            }catch (IOException i){System.out.println("Erreur");}
+        }catch (FileNotFoundException f){System.out.println("Erreur");}
         System.out.println("mon max: " + monmax);
     }
+    public static int max(int a, int b)
+    {
+        return a > b ? a : b;
+    }
 }
-        
